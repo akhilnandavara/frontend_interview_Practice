1edfc4c831ac7266cff5808cc6c97939e993a069
akhilnandavara <akhiln1108@gmail.com>
2024-04-08 23:07:30 +0530

    Add code files and notes for closures in JavaScript

--- js-interview-practice/3-closure/index.js
+++ js-interview-practice/3-closure/index.js
@@ -3,68 +3,68 @@
 // Question 1 : Lexical Scope
 
 // global scope
-// function local(){
-//     // local scope
-//     var username = 'Hello_Coder'
-//     console.log(username)
-// }
-// local()
+function local(){
+    // local scope
+    var username = 'Hello_Coder'
+    console.log(username)
+}
+local()
 
 
 // Question 2 : Closure
 
-// function makeFunc() {
-//     var name = 'Mozilla';
-//     function displayName() {
-//       alert(name);
-//     }
-//     return displayName;
-//   }
-  
-//   var myFunc = makeFunc();
-//   myFunc();
+function makeFunc() {
+    var name = 'Mozilla';
+    function displayName() {
+      alert(name);
+    }
+    return displayName;
+  }
+
+  var myFunc = makeFunc();
+  myFunc();
 
 
 // Question 3 : Closure scope chain 
 
-// const e = 10;
-// function sum(a) {
-//   return function (b) {
-//     return function (c) {
-//       // outer functions scope
-//       return function (d) {
-//         // local scope
-//         return a + b + c + d + e;
-//       };
-//     };
-//   };
-// }
+const e = 10;
+function sum(a) {
+  return function (b) {
+    return function (c) {
+      // outer functions scope
+      return function (d) {
+        // local scope
+        return a + b + c + d + e;
+      };
+    };
+  };
+}
 
-// console.log(sum(1)(2)(3)(4)); // log 20
+console.log(sum(1)(2)(3)(4)); // log 20
 
 
 // Question 4 : Output
 
-// let count = 0;
-// (function printCount(){
-//     if(count === 0){
-//         let count = 1;
-//         console.log(count); //1
-//     }
-//     console.log(count);// 0
-// })();
+(function printCount(){
+    let count = 0;
+    if(count === 0){
+        let count = 1;
+        console.log(count); //1
+    }
+    console.log(count);// 0
+})();
 
 
 // Question 5 : Write function for this addSix()
 
-// function createBase(num){
-//     return function (innerNum) {
-//         console.log(innerNum + num);
-//     };
-// }
-// var addSix = createBase(6);
-// addSix(10);
-// addSix(21);
+function createBase(num){
+    return function (innerNum) {
+        console.log(innerNum + num);
+    };
+}
+var addSix = createBase(6);
+addSix(10);
+addSix(21);
 
 
 // Question 6 : Time Optimization
@@ -93,7 +93,7 @@ function find1() {
     return function(index){
         console.log(a[index]);    
     }
-    
+
 }
 const closure = find1();
 console.time("6");
@@ -102,3 +102,93 @@ console.timeEnd("6");
 console.time("50");
 closure(50);
 console.timeEnd("50");
+
+
+// Question 7 : how to create private counter in javascript
+
+function counter(){
+    let count=0;
+    function increment(){
+        count++;
+        return count;
+    }
+    function decrement(){
+        count--;
+        return count;
+    }
+    return {
+        increment,
+        decrement
+    }
+}
+const count = counter();
+console.log(count.increment())
+console.log(count.increment())
+console.log(count.decrement())
+
+// Question 8 :what is modal pattern in javascript
+
+var modal=(function outer(){
+    function privateFunction(){
+        console.log('private function')
+    }
+    return{
+        publicFunction: function(){
+            // privateFunction();
+            console.log('public function')
+        }
+    }
+})();
+
+
+modal.publicFunction()
+modal.privateFunction()
+
+// Question 9 : once polyfill
+
+function Once(fn) {
+    let called = false;
+    let result;
+    return function (...arguments) {
+        if (!called) {
+            called = true
+            result = fn.apply(this, arguments)
+        }
+        return result
+    }
+}
+const hello= Once((a,b) => console.log("hello",a,b))
+hello(1,2)
+hello()
+
+// Question 11 : Memoize Polyfill  
+
+function myMemoize(fn,context) {
+    
+    const res = {};
+    return function(...args){
+        var argsCache = JSON.stringify(args);
+        if(!res[argsCache]){ 
+            res[argsCache] = fn.call(context || this,...args);
+        }
+        return res[argsCache];    
+    };
+}
+
+const clumsyProduct = (num1,num2) => {
+    
+    for (let i = 1; i <= 10000000; i++) {}
+    return num1 * num2;    
+}
+
+const MemoizeClumsyProduct = myMemoize(clumsyProduct);
+
+console.time("First call");
+console.log(MemoizeClumsyProduct(10,5));
+console.timeEnd("First call")
+
+console.time("Second call");
+console.log(MemoizeClumsyProduct(10,5));
+console.timeEnd("Second call");
+
+
